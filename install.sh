a83773f0f392e2f87cdbdbd7fba013353c492774
Vlad Bulimac <vbulimac@deloittece.com>
2019-05-06 15:29:19 +0300

    Enhance the messages

--- install.sh
+++ install.sh
@@ -23,16 +23,18 @@ set -eo
 SCRIPT_PATH="$( pwd )"
 MERGE_SCRIPT_PATH="${SCRIPT_PATH}/ansible-vault-merge"
 
+echo "=================================="
 echo "Checking ${MERGE_SCRIPT_PATH} file"
 if [[ ! -f "${MERGE_SCRIPT_PATH}" ]]; then
 	echo "ansible-vault-merge script not found in path ${MERGE_SCRIPT_PATH}"
 	exit 1
 fi
+echo "${MERGE_SCRIPT_PATH} already exists"
 
 pushd ~/
 
-echo "Adding attributesfile property in `pwd`/.gitconfig"
-if ! grep "attributesfile" .gitconfig ; then
+echo "Adding attributesfile property in `pwd`/.gitconfig if not exists"
+if ! grep -q "attributesfile" .gitconfig ; then
 	sed -i -E 's/\[core\]/\[core\] \
 	attributesfile = ~\/.gitattributes/g' .gitconfig 
 fi
@@ -54,13 +56,14 @@ read -d '' GIT_ATTR_CONTENT << EOM || true
 EOM
 
 echo "Checking if `pwd`/.gitconfig was previous configured"
-if ! grep -E "(?:merge \"ansible-vault\"|diff \"vault\"|diff \"ansible-vault\")" .gitconfig ; then
+if ! grep -q -E "(?:merge \"ansible-vault\"|diff \"vault\"|diff \"ansible-vault\")" .gitconfig ; then
 	echo "Configuring `pwd`/.gitconfig"
 	echo "$GIT_CONFIG_CONTENT" | tee -a .gitconfig
 fi
 
 echo "Checking if `pwd`/.gitattributes was previous configured"
-if ! grep "${GIT_ATTR_CONTENT}" .gitattributes ; then
+if ! grep -q "${GIT_ATTR_CONTENT}" .gitattributes ; then
 	echo "Configuring `pwd`/.gitattributes"
 	echo "$GIT_ATTR_CONTENT" | tee -a .gitattributes
 fi
+echo "=================================="
